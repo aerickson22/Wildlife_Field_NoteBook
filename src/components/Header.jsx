2790dce0b231a24ce2d4abf2e262008c84188be5
aerickson <aerick22@iastate.edu>
2026-06-21 20:29:05 -0500

    Added Search bar for finding a log

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,36 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useUser } from "../utils/UserContext";
-import { Person } from "react-bootstrap-icons";
-import Container from 'react-bootstrap/Container';
+import { Person, Search } from "react-bootstrap-icons";
+import { useForm } from "react-hook-form";
+import { Container, Form } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export default function Footer() {
-    const { user, setUser } = useUser();
+export default function Header() {
+    const { user } = useUser();
+    const { handleSubmit, register } = useForm();
+    const onSubmit = values => console.log(values);
 
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="/"><NavLink to="/">My Field Notebook</NavLink></Navbar.Brand>
+                <Navbar.Brand as={NavLink} to="/">My Field Notebook</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link href="/log">Log Sighting</Nav.Link>
-                        <Nav.Link href="/sighting/:id">Find a Log Sighting</Nav.Link>
-                        <Nav.Link href="/map">Map of Your Logs</Nav.Link>
-                        {user === null ? <Nav.Link href="login"><Person/> Login</Nav.Link> : <Nav.Link><Person/> user.username</Nav.Link>}
+                    <Nav className="ms-auto align-items-center">
+                        <Nav.Link as={NavLink} to="/log">Log Sighting</Nav.Link>
+                        <Nav.Link as={NavLink} to="/map">Map of Your Logs</Nav.Link>
+                        {user === null ? (
+                            <Nav.Link as={NavLink} to="/login"><Person/> Login</Nav.Link>
+                        ) : (
+                            <Nav.Link><Person/> {user.username}</Nav.Link>
+                        )}
+                        <Form onSubmit={handleSubmit(onSubmit)} className="ms-lg-3">
+                            <Form.Group className="d-flex align-items-center">
+                                <Form.Label className="me-2 mb-0"><Search/></Form.Label>
+                                <Form.Control type="text" {...register("searchQuery")} placeholder="Search for a Log...." />
+                            </Form.Group>
+                        </Form>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
