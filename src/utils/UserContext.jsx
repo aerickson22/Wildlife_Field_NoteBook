65b98956d0afaa66aa2b54d336efa49b85c8e245
aerickson <aerick22@iastate.edu>
2026-06-26 18:58:21 -0500

    added localStorage to keep users login

--- src/utils/UserContext.jsx
+++ src/utils/UserContext.jsx
@@ -1,9 +1,21 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 const UserContext = createContext(null);
 
 export function UserProvider({ children }) {
+    const USER_KEY = import.meta.env.VITE_USER_STORAGE_KEY;
     const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const data = window.localStorage.getItem(USER_KEY);
+        setUser(data ? JSON.parse(data) : null);
+    }, []);
+
+    useEffect(() => {
+        if (user !== null) {
+            window.localStorage.setItem(USER_KEY, JSON.stringify(user));
+        }
+    }, [user]);
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
