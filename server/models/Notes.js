76fd5f28439618e85fda992b649980a5c51f21e7
aerickson <aerick22@iastate.edu>
2026-08-05 14:19:10 -0500

    another soft restart

--- server/models/Notes.js
+++ server/models/Notes.js
@@ -4,52 +4,6 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const NoteSchema = new Schema({
-    date: {
-        type: Date,
-        required: true,
-    },
-    time: {
-        type: String,
-        required: true,
-    },
-    temperature_F: {
-        type: Number,
-    },
-    conditions: {
-        type: [String],
-    },
-    taxon: {
-        type: String,
-        required: true,
-    },
-    location: {
-        site: { type: String },
-        code: { type: String },
-        county: { type: String },
-        county_code: { type: String },
-    },
-    ecosystem: {
-        type_name: { type: String },
-        code: { type: String },
-    },
-    methods_of_detection: [
-        {
-        method: { type: String },
-        code: { type: String },
-        },
-    ],
-    natural_history: {
-        type: String,
-    },
-    abbreviations: {
-        type: Map,
-        of: String,
-    },
-    _user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Users',
-        require: true
-    }
 });
 
 const Notes = mongoose.model("FieldLog", NoteSchema, "FieldLog");
